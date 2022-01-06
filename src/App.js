d87cd9f603ea4c20aca526d3dbb2b3ea82436c72
ring1996 <ring199623@gmail.com>
2022-01-06 23:06:40 +0900

    [add] GitHub Actionsのテスト2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ export const App = () => {
   return (
     <div className="App">
       <h1>Hello React!</h1>
-      <p>This is GitHub Actions Test.</p>
+      <p>This is GitHub Actions Test2.</p>
     </div>
   );
 };
